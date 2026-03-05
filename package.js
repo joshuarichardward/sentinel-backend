8a5bb451c10c0f68a6597a7af9ea319db2653ffd
joshuarichardward <joshuarichardward@gmail.com>
2026-03-05 12:41:21 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "sentinel-backend",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "node --watch server.js"
+  },
+  "engines": {
+    "node": ">=18"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
